8efcef3a9c4e4b0dfff1e8bd1e967771aa5a3e97
JamesWithCode <minseok.jeong.james@gmail.com>
2022-02-04 17:49:47 +0900

    Example 2 - PersonDAO: prototype - JdbcConnection: singleton

--- src/main/java/com/james/springbeanpractice/JdbcConnection.java
+++ src/main/java/com/james/springbeanpractice/JdbcConnection.java
@@ -1,7 +1,10 @@
 package com.james.springbeanpractice;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope(value= ConfigurableBeanFactory.SCOPE_SINGLETON)
 @Component
 public class JdbcConnection {
       public JdbcConnection() {
--- src/main/java/com/james/springbeanpractice/PersonDAO.java
+++ src/main/java/com/james/springbeanpractice/PersonDAO.java
@@ -1,9 +1,12 @@
 package com.james.springbeanpractice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PersonDAO {
 
       @Autowired
