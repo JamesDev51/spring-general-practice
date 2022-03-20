61718875ef7d0871e6b4d0b44ba55d3eede9dc26
JamesWithCode <minseok.jeong.james@gmail.com>
2022-03-20 20:21:57 +0900

    spring jdbc 사용해서 data 조작하기

--- springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/SpringbootJdbcToJpaPracticeApplication.java
+++ springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/SpringbootJdbcToJpaPracticeApplication.java
@@ -1,13 +1,27 @@
 package com.james.database;
 
+import com.james.database.jdbc.PersonJdbcDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJdbcToJpaPracticeApplication {
+public class SpringbootJdbcToJpaPracticeApplication implements CommandLineRunner {
+
+      private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+      @Autowired
+      PersonJdbcDAO dao;
 
       public static void main(String[] args) {
             SpringApplication.run(SpringbootJdbcToJpaPracticeApplication.class, args);
       }
 
+      @Override
+      public void run(String... args) throws Exception {
+            logger.info("All users -> {}",dao.findAll());
+      }
 }
