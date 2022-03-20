4540cdb5fb81280783ca4a959ed3a95f294bb6c8
JamesWithCode <minseok.jeong.james@gmail.com>
2022-03-20 22:33:04 +0900

    spring jdbc row mapper 구현

--- springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/SpringbootJdbcToJpaPracticeApplication.java
+++ springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/SpringbootJdbcToJpaPracticeApplication.java
@@ -1,5 +1,6 @@
 package com.james.database;
 
+import com.james.database.entity.Person;
 import com.james.database.jdbc.PersonJdbcDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringbootJdbcToJpaPracticeApplication implements CommandLineRunner {
@@ -23,5 +26,13 @@ public class SpringbootJdbcToJpaPracticeApplication implements CommandLineRunner
       @Override
       public void run(String... args) throws Exception {
             logger.info("All users -> {}",dao.findAll());
+            logger.info("User id 10001 -> {}",dao.findById(10001));
+            logger.info("Deleting User id 10002 -> No of Rows Deleted - {}",dao.deleteById(10002));
+
+            logger.info("Inserting 10004 - {}",dao.insert(new Person(10004, "John","Berlin",new Date())));
+
+            logger.info("Updating 10003 - {}",dao.update(new Person(10003, "Tara", "NewYork",new Date())));
+
       }
+
 }
--- springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/jdbc/PersonJdbcDAO.java
+++ springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/jdbc/PersonJdbcDAO.java
@@ -4,8 +4,12 @@ import com.james.database.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -14,10 +18,55 @@ public class PersonJdbcDAO {
       @Autowired
       JdbcTemplate jdbcTemplate;
 
+      class PersonRowMapper implements  RowMapper<Person>{
+            @Override
+            public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+                  Person person = new Person();
+                  person.setId(rs.getInt("id"));
+                  person.setName(rs.getString("name"));
+                  person.setLocation(rs.getString("location"));
+                  person.setBirthDate(rs.getTimestamp("birth_date"));
+                  return person;
+            }
+      }
+
       //select * from person
       public List<Person> findAll(){
             return jdbcTemplate.query("select * from person",
                         new BeanPropertyRowMapper<>(Person.class));
       }
+      //select * from person
+      public Person findById(int id){
+            return jdbcTemplate.queryForObject("select * from person where id=? ", new Object[]{id},
+                        new BeanPropertyRowMapper<>(Person.class));
+      }
+
+      //select * from person
+      public int deleteById(int id){
+            return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+      }
+
+      //select * from person
+      public int insert(Person person){
+            return jdbcTemplate.update("INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE) VALUES(?,?,?,?);",
+                        new Object[]{
+                                    person.getId(),
+                                    person.getName(),
+                                    person.getLocation(),
+                                    new Timestamp(person.getBirthDate().getTime())});
+      }
+
+     //select * from person
+      public int update(Person person){
+            return jdbcTemplate.update("update  PERSON set name=?, location=?, birth_date=? where id=?;",
+                        new Object[]{
+                                    person.getName(),
+                                    person.getLocation(),
+                                    new Timestamp(person.getBirthDate().getTime()),
+                                    person.getId()});
+      }
+
+
+
 
 }
