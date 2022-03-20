96751426a147b0dbee3d157adef3fd220c82b9b7
JamesWithCode <minseok.jeong.james@gmail.com>
2022-03-20 18:44:08 +0900

    spring jdbc to jpa practice initial commit

---
+++ springboot-jdbc-to-jpa-practice/src/main/java/com/james/database/SpringbootJdbcToJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.james.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcToJpaPracticeApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringbootJdbcToJpaPracticeApplication.class, args);
+      }
+
+}
