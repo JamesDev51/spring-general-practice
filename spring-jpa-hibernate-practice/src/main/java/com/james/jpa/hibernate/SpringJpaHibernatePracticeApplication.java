c06364f86534fd28395650b0399b374e5c6c63f9
JamesWithCode <minseok.jeong.james@gmail.com>
2022-03-22 18:54:06 +0900

    spring jpa hibernate initial commit

---
+++ spring-jpa-hibernate-practice/src/main/java/com/james/jpa/hibernate/SpringJpaHibernatePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.james.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernatePracticeApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringJpaHibernatePracticeApplication.class, args);
+      }
+
+}
