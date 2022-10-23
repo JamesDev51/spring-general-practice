286ef2d26e8658dc3fdef8c55483539324387d14
JamesWithCode <minseok.jeong.james@gmail.com>
2022-10-23 12:56:19 +0900

    폴더명 변경

--- springboot-guide-book-practice/Chapter 5 REST API/src/main/java/com/jameswithcode/restapi/controller/PutMapping.java
+++ springboot-guide-book-practice/Chapter 5 REST API/src/main/java/com/jameswithcode/restapi/controller/PutMapping.java
@@ -3,7 +3,6 @@ package com.jameswithcode.restapi.controller;
 import com.jameswithcode.restapi.dto.MemberDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/put-api")
-public class PubMapping {
+public class PutMapping {
 
 	@RequestMapping(value="/member")
 	public String postMember(@RequestBody Map<String,Object>putData){
@@ -22,17 +21,17 @@ public class PubMapping {
 		return sb.toString();
 	}
 
-	@PutMapping(value="member1")
+	@org.springframework.web.bind.annotation.PutMapping(value="member1")
 	public String postMemberDto1(@RequestBody MemberDto memberDto){
 		return memberDto.toString();
 	}
 
-	@PutMapping(value="member2")
+	@org.springframework.web.bind.annotation.PutMapping(value="member2")
 	public MemberDto postMemberDto2(@RequestBody MemberDto memberDto){
 		return memberDto;
 	}
 
-	@PutMapping(value="/member3")
+	@org.springframework.web.bind.annotation.PutMapping(value="/member3")
 	public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
 		return ResponseEntity
 				.status(HttpStatus.ACCEPTED)
